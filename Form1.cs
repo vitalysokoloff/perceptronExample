6f8a3a5dc7e5a44a4ec105e6b557293d11ca1abe
VitalySokoloff <vitaliysokoloff@gmail.com>
2020-09-17 07:58:56 +0700

    version 1

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace perceptron
+{
+    public partial class Form1 : Form
+    {
+        private bool isLeftMouseKeyDown = false;
+
+        private Graphics graphics;
+        private Color backcolor = Color.FromArgb(255, Color.White); // цвет фона
+        private Pen pen; // карандаш
+
+        private Point CurPix; // текущая координата 
+        private Point OldPix; // захваченая координата
+
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.Black, 5);
+            pen.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
+
+            /* Создаём новый бмп */
+
+            Bitmap bmp = new Bitmap(200, 200);
+
+            /* Закрашиваем пиксели белым */
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                    bmp.SetPixel(i, j, Color.White);
+
+            pictureBox1.Image = bmp;
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.ScaleTransform(1, 1);
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isLeftMouseKeyDown = true;
+            OldPix.X = e.X;
+            OldPix.Y = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isLeftMouseKeyDown && e.X > 0 && e.Y > 0 && e.X < pictureBox1.Width && e.Y < pictureBox1.Height)
+            {
+                CurPix.X = e.X;
+                CurPix.Y = e.Y;
+                graphics.DrawLine(pen, OldPix, CurPix);
+                OldPix = CurPix;
+
+
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isLeftMouseKeyDown)
+                isLeftMouseKeyDown = false;
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            neuronListBox.SetSelected(Network.curNeuron, false);
+            inputTextBox.Text = Network.SetInput(pictureBox1.Image);
+            weightBox.Text = Network.neurons[Network.curNeuron].GetWeights();
+            answerLabel.Text = "Вы нарисовали " + Network.GetReactedNeuron();
+            weightBox.Text = Network.neurons[Network.curNeuron].GetWeights();
+            neuronBox.Text = "NEURON: " + Network.curNeuron;
+            neuronListBox.SetSelected(Network.curNeuron, true);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            pictureBox1.Invalidate();
+        }
+
+        private void neuronListBox_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            Network.curNeuron = neuronListBox.SelectedIndex;
+            weightBox.Text = Network.neurons[Network.curNeuron].GetWeights();
+            neuronBox.Text = "NEURON: " + Network.curNeuron;
+        }
+
+        private void teachButton_Click(object sender, EventArgs e)
+        {
+            Network.neurons[Network.curNeuron].Teach();
+            weightBox.Text = Network.neurons[Network.curNeuron].GetWeights();
+        }
+    }
+}
---
+++ Network.cs
@@ -0,0 +1,129 @@
+﻿using System.Drawing;
+
+namespace perceptron
+{
+    internal static class Network
+    {
+        internal static Neuron[] neurons { get; set; } = { new Neuron("Крестик"), new Neuron("Кружок"), new Neuron("Треугольник") }; // выходные нейроны
+        internal static int[] input { get; set; } = new int[100]; // нармализованный входной сигнал (входные нейроны 100 штук выдают либо 0 либо 1)
+        internal static float speed { get; } = 0.2f; // скорость обучения, чем меньше темб дольше обучается нейрон (без ней обучение чрезвучайно грубое)
+        internal static int curNeuron { get; set; } = 0; // текущий нейрон/активированный нейрон
+
+        internal static string SetInput(Image image) // задать вход и превести его к нужному виду
+        {
+            Bitmap bitmap = new Bitmap(image);
+            string returning = ""; // для отображения в текст боксе
+            int x = 0, y = 0, xMax = 0, yMax = 0; // края
+
+            for (int i = 0; i < bitmap.Width; i++) // обрезка 
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    if (x == 0 && bitmap.GetPixel(i, j).ToArgb() < -1) x = i; // -1 - это белый
+                    if (bitmap.GetPixel(i, j).ToArgb() < -1) xMax = i;
+                }
+
+            for (int i = 0; i < bitmap.Height; i++)
+                for (int j = 0; j < bitmap.Width; j++)
+                {
+                    if (y == 0 && bitmap.GetPixel(j, i).ToArgb() < -1) y = i;
+                    if (bitmap.GetPixel(j, i).ToArgb() < -1) yMax = i;
+                }
+
+            bitmap = bitmap.Clone(new Rectangle(x, y, xMax - x, yMax - y), bitmap.PixelFormat);
+            bitmap = new Bitmap(bitmap, 10, 10); // уменьшение
+            int count = 0;
+
+            for (int i = 0; i < bitmap.Width; i++) // задаём масивы инпут и ретёрнинг
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    if (bitmap.GetPixel(j, i).ToArgb() < -1)
+                    {
+                        input[count] = 1;
+                        returning += "1\t";
+                    }
+                    else
+                    {
+                        input[count] = 0;
+                        returning += "0\t";
+                    }
+                    count++;
+                }
+                returning += "\n\r";
+                returning += "\n\r";
+            }
+
+            return returning;
+        }
+        internal static string GetReactedNeuron() // поиски активированного нейрона
+        {
+            float max = 0, tmp;
+
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                tmp = neurons[i].GetOutput();
+                if (tmp > max)
+                {
+                    max = tmp;
+                    curNeuron = i;
+                }
+            } 
+            return neurons[curNeuron].GetName();
+        }
+    }
+
+    internal class Neuron // нейрон
+    {
+        private string name; // имя
+        private float[] weight = new float[100]; // веса
+
+        internal Neuron(string name)
+        {
+            this.name = name;
+        }
+
+        internal string GetName()
+        {
+            return name;
+        }
+
+        internal string GetWeights() // для отображения в гуи
+        {
+            string returning = "";
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    returning += weight[count] + "\t";
+                    count++;
+                }
+                returning += "\n\r";
+                returning += "\n\r";
+            }
+
+            return returning;
+        }
+
+        internal float GetOutput() // реакция нейрона на инпут
+        {
+            float output = 0;
+            
+            for (int i=0; i < 100; i++)
+            {
+                output += Network.input[i] * weight[i];
+            }
+
+            return output;
+        }
+
+        internal void Teach() // обучение нейрона
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                weight[i] += Network.speed * Network.input[i] * (1 - weight[i]);
+            }
+        }
+    }
+}
